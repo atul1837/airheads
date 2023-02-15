e85ea4cd679294212c0e02edf1cf8d91dba69fbb
SamraatBansal <samraatbansal7@gmail.com>
2023-02-15 17:35:45 +0530

    home images change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,246 @@
 import * as React from "react";
-import HomeImage from "../../src/assets/home.webp";
+import { isMobile } from "react-device-detect";
+import HomeImage from "../../src/assets/homeNew.jpg";
+
+import HomeImage1 from "../../src/assets/homeNft1.png"
+import HomeImage2 from "../../src/assets/homeNft2.png"
+import HomeImage3 from "../../src/assets/homeNft3.png"
+import HomeImage4 from "../../src/assets/homeNft4.png"
+import HomeImage5 from "../../src/assets/homeNft5.png"
+import HomeImage6 from "../../src/assets/homeNft6.png"
+import HomeImage7 from "../../src/assets/homeNft7.png"
+import HomeImage8 from "../../src/assets/homeNft8.png"
+import HomeImage9 from "../../src/assets/homeNft9.png"
+import HomeImage10 from "../../src/assets/homeNft10.png"
+import HomeImage11 from "../../src/assets/homeNft11.png"
+import HomeImage12 from "../../src/assets/homeNft12.png"
+import HomeImage13 from "../../src/assets/homeNft13.png"
+import HomeImage14 from "../../src/assets/homeNft14.png"
+import HomeImage15 from "../../src/assets/homeNft15.png"
+import HomeImage16 from "../../src/assets/homeNft16.png"
+import HomeImage17 from "../../src/assets/homeNft17.png"
+
+const imageData = [
+  {
+    img: {
+      src: HomeImage1,
+      // webp: HomeImage1Webp,
+      alt: "HomeImage1",
+      style: {
+        height: isMobile ? "9vw" : "8vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "24vw",
+        left: isMobile ? "11vw" : "76vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage2,
+      alt: "HomeImage2",
+      style: {
+        height: isMobile ? "10vw" : "8vw",
+        position: "absolute",
+        top: isMobile ? "83vw" : "8vw",
+        left: isMobile ? "25vw" : "50vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage3,
+      alt: "HomeImage3",
+      style: {
+        height: isMobile ? "14vw" : "12vw",
+        position: "absolute",
+        top: isMobile ? "37vw" : "2vw",
+        left: isMobile ? "11vw" : "21vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage4,
+      // webp: HomeImage1Webp,
+      alt: "HomeImage4",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage5,
+      // webp: HomeImage5Webp,
+      alt: "HomeImage5",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage6,
+      // webp: HomeImage6Webp,
+      alt: "HomeImage6",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage7,
+      // webp: HomeImage7Webp,
+      alt: "HomeImage7",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage8,
+      // webp: HomeImage8Webp,
+      alt: "HomeImage8",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage9,
+      // webp: HomeImage9Webp,
+      alt: "HomeImage9",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage10,
+      // webp: HomeImage10Webp,
+      alt: "HomeImage10",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage11,
+      // webp: HomeImage11Webp,
+      alt: "HomeImage11",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage12,
+      // webp: HomeImage12Webp,
+      alt: "HomeImage12",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage13,
+      // webp: HomeImage13Webp,
+      alt: "HomeImage13",
+      style: {
+        height: isMobile ? "11vw" : "9vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "30vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage14,
+      // webp: HomeImage14Webp,
+      alt: "HomeImage14",
+      style: {
+        height: isMobile ? "20vw" : "16vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "60vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage15,
+      // webp: HomeImage15Webp,
+      alt: "HomeImage15",
+      style: {
+        height: isMobile ? "20vw" : "16vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "60vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage16,
+      // webp: HomeImage16Webp,
+      alt: "HomeImage16",
+      style: {
+        height: isMobile ? "20vw" : "16vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "60vw",
+      },
+    },
+  },
+  {
+    img: {
+      src: HomeImage17,
+      // webp: HomeImage17Webp,
+      alt: "HomeImage17",
+      style: {
+        height: isMobile ? "20vw" : "16vw",
+        position: "absolute",
+        top: isMobile ? "137vw" : "25vw",
+        left: isMobile ? "11vw" : "60vw",
+      },
+    },
+  },
+];
 
 export default function Home() {
   return (
@@ -12,6 +253,24 @@ export default function Home() {
         height: "100vh",
         width: "100%",
       }}
-    ></div>
+    >
+      (
+        <>
+        {imageData.map((data, i) => (
+          <>
+            <picture>
+              <source srcSet={data.img.webp} />
+              <img
+                alt={data.img.alt}
+                src={data.img.src}
+                style={data.img.style}
+              />
+            </picture>
+          </>
+        ))}
+        </>
+      )
+
+    </div>
   );
 }
